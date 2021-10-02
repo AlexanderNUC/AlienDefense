397fa0d70fd4663315ecea58686a8f5911fb3659
AlexanderNUC <alexander.n@sci.tu.ac.th>
2021-10-02 16:51:28 +0700

    V3

--- Assets/Scripts/yee.cs
+++ Assets/Scripts/yee.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class lol : MonoBehaviour
+public class yee : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //here is your vesion control lmao
     }
 
     // Update is called once per frame
