69bc025d83c12b4983a0436de6d43f808898e20f
AlexanderNUC <alexander.n@sci.tu.ac.th>
2021-10-02 16:52:51 +0700

    V4

---
+++ Assets/Scripts/yee2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yee2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
